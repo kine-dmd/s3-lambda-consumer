63145c48c9f9b68db8cb927d9f4e11a77412ce80
Rohan Padmanabhan <rohan@padmanabhan.net>
2019-06-06 17:48:16 +0100

    Add parquet file handler.

---
+++ parquetHandler/parquetHandler.go
@@ -0,0 +1,76 @@
+package parquetHandler
+
+import (
+	"github.com/xitongsys/parquet-go/ParquetFile"
+	"github.com/xitongsys/parquet-go/ParquetWriter"
+	"github.com/xitongsys/parquet-go/parquet"
+	"log"
+	"runtime"
+)
+
+type ParquetFileHandler struct {
+	file   ParquetFile.ParquetFile
+	writer *ParquetWriter.ParquetWriter
+}
+
+func MakeParquetFile(filePath string, rowType interface{}) (*ParquetFileHandler, error) {
+	// Create a file to write to
+	fileWriter, err := ParquetFile.NewLocalFileWriter(filePath)
+	if err != nil {
+		log.Println("Unable to create parquet file ", err)
+		return nil, err
+	}
+
+	// Create a file writer for that file
+	cpuThreads := int64(runtime.NumCPU())
+	parquetWriter, err := ParquetWriter.NewParquetWriter(fileWriter, rowType, cpuThreads)
+	if err != nil {
+		log.Println("Unable to create parquet writer ", err)
+		return nil, err
+	}
+
+	// Use default row group size and compression codecs
+	parquetWriter.RowGroupSize = 128 * 1024 * 1024 // 128 MB
+	parquetWriter.CompressionType = parquet.CompressionCodec_SNAPPY
+
+	// Save for use
+	parqFile := new(ParquetFileHandler)
+	parqFile.writer = parquetWriter
+	parqFile.file = fileWriter
+
+	// No errors to return
+	return parqFile, nil
+}
+
+func (parqFileHandler *ParquetFileHandler) WriteData(allData []interface{}) error {
+	// Write each row to the file
+	for _, row := range allData {
+		err := parqFileHandler.writer.Write(row)
+		if err != nil {
+			log.Println("Error writing row to file ", err, row)
+			return err
+		}
+	}
+
+	// Success - no error to return
+	return nil
+}
+
+func (parqFileHandler *ParquetFileHandler) CloseFile() error {
+	// Write footer to parquet file
+	err := parqFileHandler.writer.WriteStop()
+	if err != nil {
+		log.Println("Unable to write footer to Parquet file ", err)
+		return err
+	}
+
+	// Close the file itself
+	err = parqFileHandler.file.Close()
+	if err != nil {
+		log.Println("Unable to close Parquet file ", err)
+		return err
+	}
+
+	// Success - no error to return
+	return nil
+}
